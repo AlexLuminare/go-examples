f7c7cdfc2d096d2ee273c0159135944d8b59ccc5
Alex Yersh <yersh.alex@gmail.com>
2024-09-03 11:52:40 +0300

    Добавлены примеры использования подготовленных операторов и транзакций

--- data/database.go
+++ data/database.go
@@ -6,6 +6,9 @@ import (
 	_ "modernc.org/sqlite"
 )
 
+var insertNewCategory *sql.Stmt
+var changeProductCategory *sql.Stmt
+
 func listDrivers() {
 	for _, driver := range sql.Drivers() {
 		Printfln("Driver: %v", driver)
@@ -15,6 +18,8 @@ func openDatabase() (db *sql.DB, err error) {
 	db, err = sql.Open("sqlite", "products.db")
 	if err == nil {
 		Printfln("Opened database")
+		insertNewCategory, _ = db.Prepare("INSERT INTO Categories (Name) VALUES (?)")
+		changeProductCategory, _ = db.Prepare("UPDATE Products SET Category = ? WHERE Id = ?")
 	}
 	return
 }
--- data/main.go
+++ data/main.go
@@ -16,11 +16,9 @@ type Category struct {
 	Name string
 }
 
-func queryDatabase(db *sql.DB, id int) (p Product) {
+func queryDatabaseByID(db *sql.DB, id int) (p Product) {
 	row := db.QueryRow(`
 	        SELECT Products.Id, Products.Name, Products.Price,
-	
-	
 	                Categories.Id as Cat_Id, Categories.Name as CatName 
 	                FROM Products, Categories
 	        WHERE Products.Category = Categories.Id
@@ -35,6 +33,32 @@ func queryDatabase(db *sql.DB, id int) (p Product) {
 		Printfln("Row error: %v", row.Err().Error())
 	}
 	return
+}
+
+func queryDatabaseByGroup(db *sql.DB, categoryName string) []Product {
+	products := []Product{}
+	rows, err := db.Query(`
+	        SELECT Products.Id, Products.Name, Products.Price, 
+	                Categories.Id as Cat_Id, Categories.Name as CatName
+	                FROM Products, Categories
+	        WHERE Products.Category = Categories.Id
+	            AND Categories.Name = ?`, categoryName)
+	if err == nil {
+		for rows.Next() {
+			p := Product{}
+			scanErr := rows.Scan(&p.Id, &p.Name, &p.Price,
+				&p.Category.Id, &p.Category.Name)
+			if scanErr == nil {
+				products = append(products, p)
+			} else {
+				Printfln("Scan error: %v", scanErr)
+				break
+			}
+		}
+	} else {
+		Printfln("Error: %v", err)
+	}
+	return products
 }
 
 func insertRow(db *sql.DB, p *Product) (id int64) {
@@ -52,13 +76,59 @@ func insertRow(db *sql.DB, p *Product) (id int64) {
 	return
 }
 
+func insertAndUseCategory(name string, productIDs ...int) {
+	result, err := insertNewCategory.Exec(name)
+	if err == nil {
+		newID, _ := result.LastInsertId()
+		for _, id := range productIDs {
+			changeProductCategory.Exec(int(newID), id)
+		}
+	} else {
+		Printfln("Prepared statement error: %v", err)
+	}
+}
+
+func insertAndUseCategoryTransactioned(db *sql.DB, name string, productIDs ...int) (err error) {
+	tx, err := db.Begin()
+	updatedFailed := false
+	if err == nil {
+		catResult, err := tx.Stmt(insertNewCategory).Exec(name)
+		if err == nil {
+			newID, _ := catResult.LastInsertId()
+			preparedStatement := tx.Stmt(changeProductCategory)
+			for _, id := range productIDs {
+				changeResult, err := preparedStatement.Exec(newID, id)
+				if err == nil {
+					changes, _ := changeResult.RowsAffected()
+					if changes == 0 {
+						updatedFailed = true
+						break
+					}
+				}
+			}
+		}
+	}
+	if err != nil || updatedFailed {
+		Printfln("Aborting transaction %v", err)
+		tx.Rollback()
+	} else {
+		tx.Commit()
+	}
+	return
+}
+
 func main() {
 	db, err := openDatabase()
 	if err == nil {
-		newProduct := Product{Name: "Stadium", Category: Category{Id: 2}, Price: 79500}
-		newID := insertRow(db, &newProduct)
-		p := queryDatabase(db, int(newID))
-		Printfln("New Product: %v", p)
+		// newProduct := Product{Name: "Stadium", Category: Category{Id: 2}, Price: 79500}
+		// newID := insertRow(db, &newProduct)
+		// insertAndUseCategory("Misc Products", 2)
+		insertAndUseCategoryTransactioned(db, "Misc Products", 2)
+		p := queryDatabaseByID(db, 2)
+		Printfln("Product: %v", p)
+		insertAndUseCategoryTransactioned(db, "Category_2", 100)
+		// p := queryDatabaseByID(db, int(newID))
+		// Printfln("New Product: %v", p)
 		db.Close()
 	} else {
 		panic(err)
